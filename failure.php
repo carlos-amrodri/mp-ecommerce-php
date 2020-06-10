63c7fd7b0c1232e27357d185a245e80812a8eb33
Carlos Rodriguez <carlos.amodri@gmail.com>
2020-06-10 08:18:37 -0300

    paginas de recibimiento de pago state

---
+++ failure.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Tienda e-commerce</title>
+</head>
+<body>
+    <div class="as-search-wrapper" role="main">
+    <div class="as-navtuck-wrapper">
+                <div class="as-l-fullwidth  as-navtuck" data-events="event52">
+                    <div>
+                        <div class="pd-billboard pd-category-header">
+                            <div class="pd-l-plate-scale">
+                                <div class="pd-billboard-background">
+                                    <img src="./assets/music-audio-alp-201709" alt="" width="1440" height="320" data-scale-params-2="wid=2880&amp;hei=640&amp;fmt=jpeg&amp;qlt=95&amp;op_usm=0.5,0.5&amp;.v=1503948581306" class="pd-billboard-hero ir">
+                                </div>
+                                <div class="pd-billboard-info">
+                                    <h1 class="pd-billboard-header pd-util-compact-small-18">Tienda e-commerce</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row align-items-center mt-5">
+            <div class="col align-self-center">
+                <div class="alert alert-danger text-center" role="alert">
+                    Tu pago fue realizado con éxito!
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col align-self-center">
+                <a href="index.php" class="btn btn-primary">Home</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
