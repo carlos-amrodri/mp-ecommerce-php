1673130f07d7928430272f88d2f061a7adcaf578
Carlos Rodriguez <carlos.amodri@gmail.com>
2020-06-10 18:42:21 -0300

    Otro cambio en el email

--- notificacion.php
+++ notificacion.php
@@ -21,13 +21,10 @@ $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
 
 //dirección del remitente 
-$headers .= "From: Miguel Angel Alvarez <pepito@desarrolloweb.com>\r\n"; 
+$headers .= "From: Carlos Rodriguez <maxirtelecomunicaciones@gmail.com>\r\n"; 
 
-//dirección de respuesta, si queremos que sea distinta que la del remitente 
-$headers .= "Reply-To: mariano@desarrolloweb.com\r\n"; 
 
-//ruta del mensaje desde origen a destino 
-$headers .= "Return-path: holahola@desarrolloweb.com\r\n"; 
+
 
 //direcciones que recibián copia 
 $headers .= "Cc: carlos.amrodri.dev@gmail.com\r\n"; 
