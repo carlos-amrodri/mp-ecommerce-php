393c75ff4a9b4b089ac83a3b1432ab20828665bf
Carlos Rodriguez <carlos.amodri@gmail.com>
2020-06-11 11:46:23 -0300

    Add monolog y titulo sacando monolog

--- notificacion.php
+++ notificacion.php
@@ -12,22 +12,25 @@
 </html>
 
 <?php 
-use Monolog\Logger;
 // use Monolog\Logger;
+// // use Monolog\Logger;
+// // use Monolog\Handler\StreamHandler;
 // use Monolog\Handler\StreamHandler;
-use Monolog\Handler\StreamHandler;
 
-// create a log channel
-$log = new Logger('Notificaciones');
-$log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+// // create a log channel
+// $log = new Logger('Notificaciones');
+// $log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
 
-// add records to the log
-$log->warning('Medio');
-$log->error('Esta es la prueba de notificaciones');
+// // add records to the log
+// $log->warning('Medio');
+// $log->error('Esta es la prueba de notificaciones');
 
 $cartel = "Maxi";
 
 error_log("*******Post********".print_r($cartel,true));
-
+error_log("===========  POST  ============== ".print_r($_POST, true));
+error_log("===========  REQUEST  ============== ".print_r($_REQUEST, true));
+$entityBody = file_get_contents('php://input');
+error_log("===========  TODO  ============== ".print_r($entityBody, true));
 ?>
 
