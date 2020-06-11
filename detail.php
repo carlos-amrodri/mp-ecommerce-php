c9134515b68e5f1a7b107319b3deb31d661abf8e
Carlos Rodriguez <carlos.amodri@gmail.com>
2020-06-11 17:34:58 -0300

    Ultimo intento4

--- detail.php
+++ detail.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/vendor/autoload.php';
     // Agrega credenciales
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-    MercadoPago\SDK::setPublicKey("APP_USR-7eb0138a-189f-4bec-87d1-c0504ead5626");
+    MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 
        $price = $_POST['price'];
