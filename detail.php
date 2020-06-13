438639177b921345351a1142dbf37c52f474abcc
Carlos Rodriguez <carlos.amodri@gmail.com>
2020-06-13 13:58:39 -0300

    Prueba sabado16

--- detail.php
+++ detail.php
@@ -58,7 +58,7 @@ $item->title = $_POST['title'];
 $item->quantity = $_POST['unit'];
 $item->unit_price = $_POST['price'];
 $preference->items = array($item);
-$preference->notification_url = 'https://hookb.in/LgDnRXL7w2SobGWGaLYw';
+$preference->notification_url = 'https://mercado-pago-ecommerce.herokuapp.com/ipn.php';
 $preference->external_reference = "carlos.amrodri@gmail.com.ar";
 
 $preference->payment_methods = array(
